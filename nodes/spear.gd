d7fb5bb7313e5abeb53c90f189eec5fcf7cdc5ab
PARCISZEWSKI <przemyslawapl@gmail.com>
2025-05-11 20:53:34 +0200

    Basic shit

--- nodes/spear.gd
+++ nodes/spear.gd
@@ -1,27 +1,42 @@
 extends RigidBody2D
 
+@export var throw_force: float = 5
+
 enum SPEARSTATUS {
 	ready, 
-	thrown
+	thrown,
+	returning
 }
 
-@onready var timer: Timer = $Timer
+@onready var timer: Timer = $castCharge
 
+
+var direction: Vector2
+var current_state: SPEARSTATUS = SPEARSTATUS.ready
 func _ready() -> void:
 	freeze = true
 
 
 func _process(delta: float) -> void:
-	var mouse_pos = get_global_mouse_position()
-	var direction = (mouse_pos - global_position).normalized()
-		
-	# Calculate the angle and set rotation
-	rotation = direction.angle() - PI/2
+	match current_state:
+		SPEARSTATUS.ready:
 
+			var mouse_pos = get_global_mouse_position()
+			direction = (mouse_pos - global_position).normalized()
+			rotation = direction.angle() - PI/2
+
+			if Input.is_action_just_pressed("primary"):
+				useItem()
+		SPEARSTATUS.thrown:
+			if Input.is_action_just_pressed("primary"):
+				resetItem()
 
 func useItem():
-	pass
+	freeze = false
+	apply_impulse(direction * throw_force)
 
+func resetItem():
+	pass
 
 func _on_health_component_health_zero() -> void:
 	queue_free()
