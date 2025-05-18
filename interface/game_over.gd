93c229aff9b0f6e49465c198429393e371244e0b
PARCISZEWSKI <przemyslawapl@gmail.com>
2025-05-18 20:14:38 +0200

    big baby

--- interface/game_over.gd
+++ interface/game_over.gd
@@ -1,7 +1,7 @@
 extends CanvasLayer
 
 var main_menu: PackedScene = preload("res://interface/main_menu.tscn")
-
+@export var clickSound: AudioStream
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	get_tree().paused = false
@@ -12,4 +12,5 @@ func _process(delta: float) -> void:
 
 
 func _on_start_button_down() -> void:
+	Audio.play_sound(clickSound, "Effects")
 	get_tree().change_scene_to_packed(main_menu)
--- interface/game_over_timer.gd
+++ interface/game_over_timer.gd
@@ -6,7 +6,12 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void: 
+func _process(delta: float) -> void:
+	# print(SupplyTimer.played_warning, SupplyTimer.time_left, SupplyTimer.is_stopped())
+	if not SupplyTimer.played_warning and SupplyTimer.time_left <= 8.0 and not SupplyTimer.is_stopped():
+		print("success")
+		SupplyTimer.played_warning = true
+		Audio.play_sound(load("res://assets/audio/Warning1.wav"), "Effects") 
 	if SupplyTimer.time_left:
 		$Label.text = "%0*d" % [2, SupplyTimer.time_to_minutes().x] + ":" + "%0*d" % [2, SupplyTimer.time_to_minutes().y]
 	else:
